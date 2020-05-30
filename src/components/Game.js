14d637fb7e5498c18bd2abc853574c83a5155a3e
pDk1209 <62177836+pDk1209@users.noreply.github.com>
2020-05-30 18:11:09 +0530

    Initial commit

---
+++ src/components/Game.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { calculateWinner } from "../helper";
+import Board from "./Board";
+
+const Game = () => {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [stepNumber, setStepNumber] = useState(0);
+  const [xIsNext, setXisNext] = useState(true);
+  const winner = calculateWinner(history[stepNumber]);
+  const xO = xIsNext ? "X" : "O";
+
+  const handleClick = i => {
+    const historyPoint = history.slice(0, stepNumber + 1);
+    const current = historyPoint[stepNumber];
+    const squares = [...current];
+
+    if (winner || squares[i]) return;
+
+    squares[i] = xO;
+    setHistory([...historyPoint, squares]);
+    setStepNumber(historyPoint.length);
+    setXisNext(!xIsNext);
+  };
+
+  return (
+    <>
+      <h1>Tic-TAC-TOE</h1>
+      <Board squares={history[stepNumber]} onClick={handleClick} />
+      <div>
+        <h3>{winner ? "Winner: " + winner : "Next Player: " + xO}</h3>
+      </div>
+    </>
+  );
+};
+
+export default Game;
